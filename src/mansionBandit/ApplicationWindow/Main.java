8478dcacdf5e9f787d4a3f4552b1e84f20b0a448
Theo <theocarr777@gmail.com>
2014-09-17 13:40:53 +1200

    The game application window including user input and some GUI elements.
    Functions but relies on methods that do not exist yet.

---
+++ src/mansionBandit/ApplicationWindow/Main.java
@@ -0,0 +1,83 @@
+package mansionBandit.ApplicationWindow;
+import java.awt.Point;
+
+
+public class Main {
+
+	private GameFrame gameFrame;
+	public Main(){
+		gameFrame = new GameFrame(this);
+	}
+	
+public static void main(String args[]) {
+		 Main game = new Main();
+}
+
+public Object getObjectAt(Point point) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public boolean useItemOnObject(Object objectAt) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+public boolean useItemOnNPC(Object objectAt) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+public Object getNPCAt(Point point) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public boolean atEmptyInventorySlot(Point point) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+public void addToInventory(Item draggingItem) {
+	// TODO Auto-generated method stub
+	
+}
+
+public void dropObject(Point point) {
+	// TODO Auto-generated method stub
+	
+}
+
+public Object getInstanceAt(Point point) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public String getDescription(Object instanceAt) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public Item getItemAt(Point point) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public void moveForward() {
+	// TODO Auto-generated method stub
+	
+}
+
+public void turnRight() {
+	// TODO Auto-generated method stub
+	
+}
+
+public void turnLeft() {
+	// TODO Auto-generated method stub
+	
+}
+
+
+
+}
