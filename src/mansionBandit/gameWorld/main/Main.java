d0a193948c12f408c0ffb2312cf5d5bdde5fb5b5
Theo <theocarr777@gmail.com>
2014-09-28 00:20:29 +1200

    Forgot to commit this with the last commit.

--- src/mansionBandit/gameWorld/main/Main.java
+++ src/mansionBandit/gameWorld/main/Main.java
@@ -23,25 +23,7 @@ public class Main {
 		new RoomsLayout(20);
 	}
 	
-	/**
-	 * This method allows a client to connect to a specified server using the address
-	 * and port number.
-	 * @param address Address of the server.
-	 * @param port Port specified by server for communication.
-	 * @param username Identifier for the client attempting to connect
-	 * @throws IOException
-	 */
-	private static void runClient(String address, int port, String username) throws IOException {
-		Socket s = new Socket(address, port);
-		System.out.println("Client connecting to: " + address + " on port: " + port);
-		new Client(s, username).start();
-	}
 	
-	//TODO: this method requires a game world parameter
-	private static void runServer(int port, int nclients) {
-		System.out.println("Creating server on port " + port + " with " + nclients + " limit");
-		new Server(port, nclients).start();
-	}
 	
 	public static void main(String[] args){
 		new Main();
