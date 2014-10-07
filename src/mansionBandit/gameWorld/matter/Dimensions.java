55e410eece1ee77af35cb9b1c56a46e4361eda49
shreyasrama <shreyas.rama1@gmail.com>
2014-10-07 15:29:06 +1300

    Completed serializable implementation, server now sends a grid (completely initialized) of which the client can receive over the stream. Also changed ItemUpdateMessage to ItemPickupMessage in order to include room changes

--- src/mansionBandit/gameWorld/matter/Dimensions.java
+++ src/mansionBandit/gameWorld/matter/Dimensions.java
@@ -1,12 +1,14 @@
 package mansionBandit.gameWorld.matter;
 
+import java.io.Serializable;
+
 /**
  * The dimensions class used to find out position and size of
  * every item
  * @author Liam De Grey
  *
  */
-public class Dimensions {
+public class Dimensions implements Serializable {
 	private int x;
 	private int y;
 	private int scale;
