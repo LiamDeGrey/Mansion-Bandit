932418452eebf67b925d466e000515a8068ddfee
Andrew Fone <fone.andrew@gmail.com>
2014-10-14 21:23:48 +1300

    added a couple more tests for factory

--- src/tests/factoryTests.java
+++ src/tests/factoryTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import mansionBandit.factory.*;
+import mansionBandit.gameWorld.areas.Hallway;
 import mansionBandit.gameWorld.areas.MansionArea;
 import mansionBandit.gameWorld.areas.Room;
 import mansionBandit.gameWorld.matter.Door;
@@ -12,12 +13,13 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class factoryTests {
-
+	/*
+	 * as the factory is random, a lot of tests use many iterations
+	 */
+	
 	@Test
 	public void testWallpapers(){
 		//ensure factory applies textures
-		//as the factory is run on randomness, use brute force to try and
-		//fail an assertertion
 		RoomFactory factory = new RoomFactory();
 		for (int i = 0; i < 100; i++){
 			Room room = new Room("");
@@ -30,7 +32,7 @@ public class factoryTests {
 	
 	@Test
 	public void testEachWallForItem(){
-		//make sure each wall is given only one item
+		//make sure each wall is given only one item (including the ceiling)
 		RoomFactory factory = new RoomFactory();
 		for (int i = 0; i < 100; i++){
 			Room room = new Room("");
@@ -38,6 +40,7 @@ public class factoryTests {
 			int south = 0;
 			int east = 0;
 			int west = 0;
+			int ceiling = 0;
 			factory.populateRoom(room);
 			for (GameMatter item : room.getItems()){
 				if (item.getFace() == Face.NORTHERN){
@@ -52,11 +55,15 @@ public class factoryTests {
 				if (item.getFace() == Face.EASTERN){
 					east++;
 				}
+				if (item.getFace() == Face.CEILING){
+					ceiling++;
+				}
 			}
-			assertTrue("north wall should have an item", north == 1);
-			assertTrue("west wall should have an item", west == 1);
-			assertTrue("south wall should have an item", south == 1);
-			assertTrue("east wall should have an item", east == 1);
+			assertTrue("north wall should have exactly one item", north == 1);
+			assertTrue("west wall should have exactly one item", west == 1);
+			assertTrue("south wall should have exactly one item", south == 1);
+			assertTrue("east wall should have exactly one item", east == 1);
+			assertTrue("the ceiling should have exactly one item", ceiling == 1);
 		}
 	}
 	
@@ -65,7 +72,7 @@ public class factoryTests {
 		//make sure each wall that is linked to another room has a door on it
 		RoomFactory factory = new RoomFactory();
 		for (int i = 0; i < 100; i++){
-			MansionArea[][] grid = getTestGrid();
+			MansionArea[][] grid = getRoomsTestGrid();
 			for (int a = 0; a < grid.length; a++){
 				for (int b = 0; b < grid[0].length; b++){
 					factory.populateRoom(grid[a][b]);
@@ -94,7 +101,7 @@ public class factoryTests {
 		//this time there are four rooms that are linked together
 		RoomFactory factory = new RoomFactory();
 		for (int i = 0; i < 100; i++){
-			MansionArea[][] grid = getTestGrid();
+			MansionArea[][] grid = getRoomsTestGrid();
 			for (int a = 0; a < grid.length; a++){
 				for (int b = 0; b < grid[0].length; b++){
 					int north = 0;
@@ -125,11 +132,84 @@ public class factoryTests {
 		}
 	}
 	
-	//TODO test hallways for doors
-	//TODO test the floors
-	//TODO test the ceilings
+	@Test
+	public void ensureHallNoItemsBetween(){
+		//make sure each wall that is linked to another room has a door on it
+		RoomFactory factory = new RoomFactory();
+		for (int i = 0; i < 100; i++){
+			MansionArea[][] grid = getRoomAndHallTestGrid();
+			for (int a = 0; a < grid.length; a++){
+				for (int b = 0; b < grid[0].length; b++){
+					//loop through the rooms
+					if (!(grid[a][b] instanceof Hallway)){
+						//skip non-hallways
+						continue;
+					}
+										
+					Boolean north = false;
+					Boolean south = false;
+					Boolean west = false;
+					Boolean east = false;
+					
+					//check to see if any walls have links to another hallway
+					if (grid[a][b].getNorth() instanceof Hallway){
+						north = true;
+					}
+					if (grid[a][b].getWest() instanceof Hallway){
+						west = true;
+					}
+					if (grid[a][b].getEast() instanceof Hallway){
+						east = true;
+					}
+					if (grid[a][b].getSouth() instanceof Hallway){
+						south = true;
+					}
+					
+					//call factory
+					factory.populateRoom(grid[a][b]);
+					
+					for (GameMatter item : grid[a][b].getItems()){
+						//go through all the objects on the walls, and make sure they arent on a wall with a links to hallway
+						if (item.getFace() == Face.NORTHERN){
+							assertFalse("item should not be on wall between hallways", north);
+						}
+						if (item.getFace() == Face.EASTERN){
+							assertFalse("item should not be on wall between hallways", east);
+						}
+						if (item.getFace() == Face.SOUTHERN){
+							assertFalse("item should not be on wall between hallways", south);
+						}
+						if (item.getFace() == Face.WESTERN){
+							assertFalse("item should not be on wall between hallways", west);
+						}
+					}
+				}
+			}
+		}
+	}
 	
-	private MansionArea[][] getTestGrid(){
+	//TODO test the floors
+	
+	private MansionArea[][] getRoomAndHallTestGrid(){
+		MansionArea[][] grid = new MansionArea[2][2];
+		
+		// H H
+		// R H
+		
+		//create rooms
+		grid[0][0] = new Hallway("");
+		grid[0][1] = new Hallway("");
+		grid[1][0] = new Room("");
+		grid[1][1] = new Hallway("");
+		
+		return linkTestGrid(grid);
+	}
+	
+	/**
+	 * generates a grid containing 4 rooms
+	 * 
+	 */
+	private MansionArea[][] getRoomsTestGrid(){
 		MansionArea[][] grid = new MansionArea[2][2];
 		
 		//create rooms
@@ -138,12 +218,20 @@ public class factoryTests {
 		grid[1][0] = new Room("");
 		grid[1][1] = new Room("");
 		
+		return linkTestGrid(grid);
+	}
+
+	/**
+	 * links a grid together (assumes [2][2] in size)
+	 *
+	 */
+	private MansionArea[][] linkTestGrid(MansionArea[][] grid){
 		//set links
 		grid[0][0].setLinks(null, grid[0][1], grid[1][0], null);
 		grid[0][1].setLinks(null, null, grid[1][1], grid[0][0]);
 		grid[1][0].setLinks(grid[0][0], grid[1][0], null, null);
 		grid[1][1].setLinks(grid[0][1], null, null, grid[1][1]);
-		
+
 		return grid;
 	}
 }
