6bf8650c892c813a59cccacaa86a9cc639aa78c4
foneandrew <fone.andrew@gmail.com>
2014-10-14 15:13:10 +1300

    removed print code

--- src/mansionBandit/gameWorld/matter/Knife.java
+++ src/mansionBandit/gameWorld/matter/Knife.java
@@ -4,9 +4,6 @@ public class Knife extends Grabable{
 
 	public Knife(String name, String description, String image, Face face, Dimensions position) {
 		super(name, description, image, face, position);
-		System.out.println("name: " + name
-				+ "description: " + description
-				+ "face: " + face);
 	}
 
 	@Override
